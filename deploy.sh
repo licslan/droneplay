79fb01e2e5433b79a49546f3292a3fb146d9e0e5
licslan <licslan@sina.com>
2019-07-23 20:40:25 +0800

    xx

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "let`s deploy java project~~~"
-sleep 3
-dir=pwd
-cd $dir
-mvn clean package -Dmaven.test.skip=true
-java -jar target/drone-0.0.1-SNAPSHOT.jar
-echo "deploy was done!"
